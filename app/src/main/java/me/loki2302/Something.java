8fdf2fbe55a0937f84a835152a6e892a05b60a40
loki2302 <loki2302@loki2302.me>
2013-11-23 12:47:21 +0400

    Initial version

---
+++ app/src/main/java/me/loki2302/Something.java
@@ -0,0 +1,5 @@
+package me.loki2302;
+
+@GenerateFactory
+public class Something {        
+}
---
+++ processor/src/main/java/me/loki2302/FactoryGeneratorProcessor.java
@@ -0,0 +1,56 @@
+package me.loki2302;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
+import javax.tools.JavaFileObject;
+
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
+
+@SupportedAnnotationTypes("me.loki2302.GenerateFactory")
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
+public class FactoryGeneratorProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        STGroup templateGroup = new STGroupFile("processor.stg");
+        
+        System.out.println(annotations);
+        
+        for(Element elem : roundEnv.getElementsAnnotatedWith(GenerateFactory.class)) {
+            System.out.println(elem.getSimpleName());
+            
+            if(elem.getKind() == ElementKind.CLASS) {
+                TypeElement classElement = (TypeElement)elem;
+                PackageElement packageElement = (PackageElement)classElement.getEnclosingElement();
+                try {
+                    JavaFileObject javaFileObject = processingEnv.getFiler().createSourceFile(classElement.getQualifiedName() + "Factory");
+                    
+                    ST generatedClassTemplate = templateGroup.getInstanceOf("factoryClass");
+                    generatedClassTemplate.add("packageName", packageElement.getQualifiedName());
+                    generatedClassTemplate.add("className", classElement.getSimpleName());
+                   
+                    Writer writer = javaFileObject.openWriter();
+                    writer.append(generatedClassTemplate.render());
+                    writer.flush();
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }       
+        
+        return true;
+    }        
+}
