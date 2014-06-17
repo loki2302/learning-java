94da3369b9a024c2f199cf6c7c1ce88cbdebe215
loki2302 <loki2302@loki2302.me>
2014-06-17 12:53:49 -0700

    Added dummy test for streams

---
+++ src/test/java/me/loki2302/StreamTest.java
@@ -0,0 +1,23 @@
+package me.loki2302;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class StreamTest {
+    @Test
+    public void dummy() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        List<Integer> doubledNumbers = numbers.stream()
+                .map(x -> 2 * x)
+                .collect(Collectors.toList());
+        assertEquals(3, doubledNumbers.size());
+        assertEquals(2, (int)doubledNumbers.get(0));
+        assertEquals(4, (int)doubledNumbers.get(1));
+        assertEquals(6, (int)doubledNumbers.get(2));
+    }
+}
