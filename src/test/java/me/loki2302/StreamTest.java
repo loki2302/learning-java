cd97f06e45d7aa197a6ff7c93777aab6e1230fa6
loki2302 <loki2302@loki2302.me>
2014-06-19 10:02:44 -0700

    Added test for reduce()

--- src/test/java/me/loki2302/StreamTest.java
+++ src/test/java/me/loki2302/StreamTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 
 public class StreamTest {
     @Test
-    public void dummy() {
+    public void canUseMap() {
         List<Integer> numbers = Arrays.asList(1, 2, 3);
         List<Integer> doubledNumbers = numbers.stream()
                 .map(x -> 2 * x)
@@ -19,5 +19,12 @@ public class StreamTest {
         assertEquals(2, (int)doubledNumbers.get(0));
         assertEquals(4, (int)doubledNumbers.get(1));
         assertEquals(6, (int)doubledNumbers.get(2));
+    }
+
+    @Test
+    public void canUseReduce() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        int sum = numbers.stream().reduce(0, (s, x) -> s + x);
+        assertEquals(6, sum);
     }
 }
