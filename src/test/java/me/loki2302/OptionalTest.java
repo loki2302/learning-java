d28f17f5298a5368034485e2ea4ca6232d24633c
loki2302 <loki2302@loki2302.me>
2014-12-03 08:45:10 +0300

    Added more tests to OptionalTest

--- src/test/java/me/loki2302/OptionalTest.java
+++ src/test/java/me/loki2302/OptionalTest.java
@@ -2,14 +2,63 @@ package me.loki2302;
 
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Function;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class OptionalTest {
     @Test
-    public void dummy() {
+    public void canUseOfNullable() {
+        Optional<String> optional = Optional.ofNullable(null);
+        assertEquals(Optional.empty(), optional);
+        assertFalse(optional.isPresent());
+
+        optional = Optional.ofNullable("hello");
+        assertEquals("hello", optional.get());
+        assertTrue(optional.isPresent());
+    }
+
+    @Test
+    public void optionalThrowsWhenTryingToGetValueOfEmptyOptional() {
+        Optional<String> emptyOptional = Optional.ofNullable(null);
+        try {
+            emptyOptional.get();
+            fail();
+        } catch (NoSuchElementException e) {
+        }
+    }
+
+    @Test
+    public void optionalThrowsWhenSupplyingNullValueToOf() {
+        try {
+            Optional.of(null);
+            fail();
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @Test
+    public void canUseFilter() {
+        // empty, because there's no value at all
+        Optional<Integer> optional = Optional.ofNullable(null);
+        Optional<Integer> filteredOptional = optional.filter(i -> i % 2 == 0);
+        assertEquals(Optional.empty(), filteredOptional);
+
+        //empty, because doesn't match the predicate
+        optional = Optional.of(1);
+        filteredOptional = optional.filter(i -> i % 2 == 0);
+        assertEquals(Optional.empty(), filteredOptional);
+
+        // not empty, because not empty and matches the predicate
+        optional = Optional.of(2);
+        filteredOptional = optional.filter(i -> i % 2 == 0);
+        assertEquals(optional, filteredOptional);
+    }
+
+    @Test
+    public void canUseOrElse() {
         Function<String, String> getOriginalOrEmpty = s -> Optional.ofNullable(s).orElse("<empty>");
 
         assertEquals("hello", getOriginalOrEmpty.apply("hello"));
