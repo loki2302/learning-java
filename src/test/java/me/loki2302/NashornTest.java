e80394b7f9d3087e1aa7849dc6d8bdad0afc1c5d
loki2302 <loki2302@loki2302.me>
2014-12-06 12:36:22 +0300

    Added canExportObjectsFromJavaToJavaScript() test

--- src/test/java/me/loki2302/NashornTest.java
+++ src/test/java/me/loki2302/NashornTest.java
@@ -2,9 +2,10 @@ package me.loki2302;
 
 import org.junit.Test;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,5 +16,19 @@ public class NashornTest {
         ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("nashorn");
         int result = (Integer)scriptEngine.eval("2 + 3");
         assertEquals(5, result);
+    }
+
+    @Test
+    public void canExportObjectsFromJavaToJavaScript() throws ScriptException {
+        List<String> myList = new ArrayList<>();
+
+        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+        ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("nashorn");
+        scriptEngine.put("myList", myList);
+        scriptEngine.eval("myList.add('hello'); myList.add('there');");
+
+        assertEquals(2, myList.size());
+        assertEquals("hello", myList.get(0));
+        assertEquals("there", myList.get(1));
     }
 }
