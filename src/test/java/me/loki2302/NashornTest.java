2e4ec3278c2e0f14b5d733452e1fa2e3be4c0e30
loki2302 <loki2302@loki2302.me>
2014-12-06 12:44:14 +0300

    Added canExportObjectsFromJavaScriptToJava() test

--- src/test/java/me/loki2302/NashornTest.java
+++ src/test/java/me/loki2302/NashornTest.java
@@ -31,4 +31,20 @@ public class NashornTest {
         assertEquals("hello", myList.get(0));
         assertEquals("there", myList.get(1));
     }
+
+    @Test
+    public void canExportObjectsFromJavaScriptToJava() throws ScriptException {
+        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+        ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("nashorn");
+        scriptEngine.eval("function add(a, b) { return a + b; }");
+        scriptEngine.eval("function sub(a, b) { return a - b; }");
+        JSCalculator jsCalculator = ((Invocable)scriptEngine).getInterface(JSCalculator.class);
+        assertEquals(5, jsCalculator.add(2, 3));
+        assertEquals(-1, jsCalculator.sub(2, 3));
+    }
+
+    public static interface JSCalculator {
+        int add(int a, int b);
+        int sub(int a, int b);
+    }
 }
