f6f01eaae0ee748be009c1115a75db2fa92bb835
loki2302 <loki2302@loki2302.me>
2016-10-09 23:50:38 -0400

    Added ProxyTest

---
+++ src/test/java/me/loki2302/ProxyTest.java
@@ -0,0 +1,57 @@
+package me.loki2302;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProxyTest {
+    @Test
+    public void dummy() {
+        Calculator calculator = new CalculatorImpl();
+        DummyInvocationHandler dummyInvocationHandler = new DummyInvocationHandler(calculator);
+
+        Calculator calculatorProxy = (Calculator)Proxy.newProxyInstance(
+                ProxyTest.class.getClassLoader(),
+                new Class<?>[] { Calculator.class },
+                dummyInvocationHandler);
+
+        int result = calculatorProxy.addNumbers(2, 3);
+        assertEquals(5, result);
+
+        assertEquals(1, dummyInvocationHandler.getInvocationCount());
+    }
+
+    public interface Calculator {
+        int addNumbers(int x, int y);
+    }
+
+    public static class CalculatorImpl implements Calculator {
+        public int addNumbers(int x, int y) {
+            return x + y;
+        }
+    }
+
+    public static class DummyInvocationHandler implements InvocationHandler {
+        private final Object target;
+        private int invocationCount;
+
+        public DummyInvocationHandler(Object target) {
+            this.target = target;
+        }
+
+        public int getInvocationCount() {
+            return invocationCount;
+        }
+
+        @Override
+        public Object invoke(Object o, Method method, Object[] args) throws Throwable {
+            ++invocationCount;
+            Object result = method.invoke(target, args);
+            return result;
+        }
+    }
+}
