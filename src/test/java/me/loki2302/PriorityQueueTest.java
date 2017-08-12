21fffaa82259ea7cdd0cae3e7cc9dfe5f2b0af21
loki2302 <loki2302@loki2302.me>
2017-08-11 22:48:09 -0400

    Added PriorityQueueTest

---
+++ src/test/java/me/loki2302/PriorityQueueTest.java
@@ -0,0 +1,42 @@
+package me.loki2302;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import static org.junit.Assert.assertEquals;
+
+public class PriorityQueueTest {
+    @Test
+    public void dummy() {
+        PriorityQueue<Task> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(task -> task.priority));
+        priorityQueue.add(new Task(2, "a"));
+        priorityQueue.add(new Task(1, "b"));
+        priorityQueue.add(new Task(3, "c"));
+        priorityQueue.add(new Task(2, "d"));
+        priorityQueue.add(new Task(3, "e"));
+
+        String s = "";
+        while(!priorityQueue.isEmpty()) {
+            s += priorityQueue.poll();
+        }
+
+        assertEquals("1b2a2d3c3e", s);
+    }
+
+    public static class Task {
+        public final int priority;
+        public final String description;
+
+        public Task(int priority, String description) {
+            this.priority = priority;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%d%s", priority, description);
+        }
+    }
+}
