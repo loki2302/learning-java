31e4ffb3ddd178ee247bae63ef081a422df52717
loki2302 <loki2302@loki2302.me>
2017-09-28 22:37:27 -0400

    Self-explanatory tests for Awaitility

--- src/test/java/me/loki2302/AwaitilityTest.java
+++ src/test/java/me/loki2302/AwaitilityTest.java
@@ -1,37 +1,87 @@
 package me.loki2302;
 
+import org.awaitility.core.ConditionEvaluationLogger;
+import org.awaitility.core.ConditionTimeoutException;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.TimeUnit;
 
-import static org.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.equalTo;
+import static org.awaitility.Awaitility.with;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class AwaitilityTest {
     @Test
-    public void dummy() {
-        AtomicInteger someCounter = new AtomicInteger(0);
-
+    public void shouldFailOnTimeout() {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.submit(() -> {
-            for(int i = 0; i < 3; ++i) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {}
+        try {
+            TodoList todoList = new TodoList(executorService, 3000);
 
-                someCounter.incrementAndGet();
+            todoList.addTodo("get coffee!!!");
+            with()
+                    .pollInterval(1, TimeUnit.SECONDS)
+                    .timeout(2, TimeUnit.SECONDS)
+                    .await().until(() -> todoList.getTodos(), hasSize(1));
+            fail();
+        } catch(ConditionTimeoutException e) {
+            // TODO: intentionally blank
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @Test
+    public void shouldWorkFineWhenThereIsEnoughTime() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            TodoList todoList = new TodoList(executorService, 3000);
+
+            todoList.addTodo("get coffee!!!");
+            Set<String> todos = with()
+                    .pollInterval(1, TimeUnit.SECONDS)
+                    .timeout(4, TimeUnit.SECONDS)
+                    .await().until(() -> todoList.getTodos(), hasSize(1));
+            assertEquals(1, todos.size());
+            assertThat(todos, contains("get coffee!!!"));
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    private static class TodoList {
+        private final ExecutorService executorService;
+        private final int sleep;
+        private final Set<String> todos = new HashSet<>();
+
+        private TodoList(ExecutorService executorService, int sleep) {
+            this.executorService = executorService;
+            this.sleep = sleep;
+        }
+
+        public void addTodo(String todo) {
+            executorService.submit(() -> {
+                sleep();
+                todos.add(todo);
+            });
+        }
+
+        public Set<String> getTodos() {
+            return new HashSet<>(todos);
+        }
+
+        private void sleep() {
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                throw new RuntimeException();
             }
-        });
-
-        await().until(() -> someCounter.get() == 3);
-        await().until(() -> someCounter.get(), equalTo(3));
-        await().untilAtomic(someCounter, equalTo(3));
-
-        executorService.shutdown();
-
-        assertEquals(3, someCounter.get());
+        }
     }
 }
