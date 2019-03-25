ab12ed433f38791ac4deac9e0bd37077fc847f24
agibalov <andrey.agibalov@gmail.com>
2019-03-24 21:48:39 -0400

    Add IteratorTest

---
+++ basic/src/test/java/me/loki2302/IteratorTest.java
@@ -0,0 +1,24 @@
+package me.loki2302;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class IteratorTest {
+    @Test
+    public void canUseListIterator() {
+        List<Integer> items = Arrays.asList(11, 22, 33);
+        Iterator<Integer> it = items.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(11, (int)it.next());
+        assertTrue(it.hasNext());
+        assertEquals(22, (int)it.next());
+        assertTrue(it.hasNext());
+        assertEquals(33, (int)it.next());
+        assertFalse(it.hasNext());
+    }
+}
